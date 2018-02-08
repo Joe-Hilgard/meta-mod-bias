16e11f9f2c4fe886d4149d132a8245a754c62396
Joe-Hilgard <jhilgard@gmail.com>
2018-02-08 17:45:10 -0600

    Substantial clean-up. -Moved functions to hilgard_functions.R -Gussied up code. -Switched to contrast codes.

---
+++ hilgard_functions.R
@@ -0,0 +1,158 @@
+# This file creates four functions
+# modMA() runs dataMA() three times to create a dataset with meta-moderators
+# inspectMA() fits the various models to the output of modMA() & retrieves stats
+# runStudy() performs modMA() and inspectMA() in a loop
+# summarize_run() condenses runStudy() output into means and power rates
+
+
+# Make a function that runs dataMA three times,
+# once for each population,
+# then mixes them together
+# TODO: Note that other parameters might be worth exploring
+#       Expand argument assignment and passing
+modMA <- function(k, d, QRP, sel, propB) {
+  d1 <- dataMA(k = k, 
+               QRP = QRP, sel = sel, propB = propB, 
+               meanD = d[1], sigma = 0,
+               cbdv = .5, maxN = 200, minN = 20,
+               meanN = 50, sdN = 20,
+               multDV = 0, out = 0, mod = 0,
+               colLim = 0, add = 0, verbose = T)
+  d2 <- dataMA(k = k, 
+               QRP = QRP, sel = sel, propB = propB, 
+               meanD = d[2], sigma = 0,
+               cbdv = .5, maxN = 200, minN = 20,
+               meanN = 50, sdN = 20,
+               multDV = 0, out = 0, mod = 0,
+               colLim = 0, add = 0, verbose = T)
+  d3 <- dataMA(k = k, 
+               QRP = QRP, sel = sel, propB = propB, 
+               meanD = d[3], sigma = 0,
+               cbdv = .5, maxN = 200, minN = 20,
+               meanN = 50, sdN = 20,
+               multDV = 0, out = 0, mod = 0,
+               colLim = 0, add = 0, verbose = T)
+  # combine three subgroups into dataset, label subgroups with "id"
+  data.out <- bind_rows(d1, d2, d3, .id = "id")
+  # convert "id" to contr.sum coding
+  data.out$id <- factor(data.out$id)
+  contrasts(data.out$id) <- contr.sum 
+  # output simulated dataset
+  return(data.out)
+}
+
+# function to analyze results of dataMA
+# TODO: fetch SE of parameters for CI inspection?
+inspectMA <- function(dataset) {
+  # basic model
+  rmamod <- rma(yi = d, sei = se, data = dataset)
+  # test for moderator
+  moderation.test <- rma(yi = d, sei = se, 
+                         mods = ~id, data = dataset)
+  # test for small-study effect
+  egger.PET.test <- rma(yi = d, sei = se,
+                        mods = ~se, data = dataset)
+  # test for moderator after adjustment for small-study
+  joint.test.additive <- rma(yi = d, sei = se,
+                             mods = ~id + se, data = dataset)
+  joint.test.interactive <- rma(yi = d, sei = se,
+                                mods = ~id * se, data = dataset)
+  # not sure how to interpret these parameters in the interactive model
+  # test for moderator in hedges & vevea weight model
+  # return test results
+  out = data.frame(d.obs = summary(rmamod)$b[1],
+                   se.obs = summary(rmamod)$se[1],
+                   d.p = summary(rmamod)$pval[1],
+                   # Moderators
+                   # Note that .1 is the intercept, reference group
+                   mod.b.obs.1 = summary(moderation.test)$b[1],
+                   mod.b.obs.2 = summary(moderation.test)$b[2],
+                   mod.b.obs.3 = summary(moderation.test)$b[3],
+                   mod.p.1 = summary(moderation.test)$pval[1],
+                   mod.p.2 = summary(moderation.test)$pval[2],
+                   mod.p.3 = summary(moderation.test)$pval[3],
+                   # Egger / PET
+                   d.obs.pet = summary(egger.PET.test)$b[1],
+                   p.pet = summary(egger.PET.test)$pval[1],
+                   b.egger = summary(egger.PET.test)$b[2],
+                   p.egger = summary(egger.PET.test)$pval[2],
+                   # joint PET-RMA tests
+                   #Additive model 
+                   joint.add.b1 = summary(joint.test.additive)$b[1],
+                   joint.add.b2 = summary(joint.test.additive)$b[2],
+                   joint.add.b3 = summary(joint.test.additive)$b[3],
+                   joint.add.b.egger = summary(joint.test.additive)$b[4],
+                   joint.add.p1 = summary(joint.test.additive)$pval[1],
+                   joint.add.p2 = summary(joint.test.additive)$pval[2],
+                   joint.add.p3 = summary(joint.test.additive)$pval[3],
+                   joint.add.p.egger = summary(joint.test.additive)$pval[4],
+                   #Interactive model
+                   joint.inter.b1 = summary(joint.test.interactive)$b[1],
+                   joint.inter.b2 = summary(joint.test.interactive)$b[2],
+                   joint.inter.b3 = summary(joint.test.interactive)$b[3],
+                   joint.inter.b1.egger = summary(joint.test.interactive)$b[4],
+                   joint.inter.b2.egger = summary(joint.test.interactive)$b[5],
+                   joint.inter.b3.egger = summary(joint.test.interactive)$b[6],
+                   joint.inter.p1 = summary(joint.test.interactive)$pval[1],
+                   joint.inter.p2 = summary(joint.test.interactive)$pval[2],
+                   joint.inter.p3 = summary(joint.test.interactive)$pval[3],
+                   joint.inter.p1.egger = summary(joint.test.interactive)$pval[4],
+                   joint.inter.p2.egger = summary(joint.test.interactive)$pval[5],
+                   joint.inter.p3.egger = summary(joint.test.interactive)$pval[6]
+  )
+  return(data.frame(out))
+}
+
+# function for performing modMA() and inspectMA() nSim number of times,
+# storing the output in a data frame.
+runStudy <- function(nSim, k, d, sel, propB, QRP) {
+  output <- NULL; 
+  for (i in 1:nSim) {
+    # make a dataset of simulated studies
+    # k studies each, effect sizes given by d
+    tempdata <- modMA(k = k, d = d, 
+                      sel = sel, propB = propB, QRP = QRP)
+    # fit our various meta-analytic models to the simulated data
+    tempresult <- inspectMA(tempdata)
+    # add this iteration's results to the output object
+    output <- bind_rows(output, tempresult)
+  }
+  output
+}
+
+# functions for summarizing a simulation series in terms of mean estimates and
+#    error rates
+# TODO: expand and refine to capture ME, RMSE, power, additive and interactive models
+# TODO: Might separate into two functions... a little hard on my eyes as it is with both estimates and p-vals
+# TODO: is there any way to get an ANOVA-like output rather than pairwise t-test?
+is_sig <- function(x) x < .05
+summarize_run <- function(x) {
+  # Get estimates of d, moderator parameters, bias parameter, bias-adjusted PET
+  x.est <- x %>% 
+    summarize_at(.vars = vars(d.obs, mod.b.obs.1:mod.b.obs.3, d.obs.pet, 
+                              joint.add.b1:joint.add.b3, joint.inter.b1:joint.inter.b3),
+                 .funs = funs(mean)) %>% 
+    # make cell means (NOTE: ASSUMES CONTRAST CODING)
+    # Might be a simpler way... lsmeans package?
+    # use transmute to drop all other columns
+    transmute(d.obs,
+              d.obs.pet,
+              d1.obs = mod.b.obs.1 + mod.b.obs.2,
+              d2.obs = mod.b.obs.1 + mod.b.obs.3,
+              d3.obs = mod.b.obs.1 - mod.b.obs.2 - mod.b.obs.3, 
+              joint.add.d1.obs = joint.add.b1 + joint.add.b2,
+              joint.add.d2.obs = joint.add.b1 + joint.add.b3,
+              joint.add.d3.obs = joint.add.b1 - joint.add.b2 - joint.add.b3,
+              joint.inter.d1.obs = joint.inter.b1 + joint.inter.b2,
+              joint.inter.d2.obs = joint.inter.b1 + joint.inter.b3,
+              joint.inter.d3.obs = joint.inter.b1 - joint.inter.b2 - joint.inter.b3)
+
+  # Get power (or Type I) rates for each p-value test
+  x.pow <- x %>% 
+    summarize_at(.vars = vars(d.p, mod.p.1:mod.p.3, 
+                              joint.add.p1:joint.add.p3,
+                              joint.inter.p1:joint.inter.p3),
+                 .funs = funs(mean(is_sig(.))))
+  
+  return(bind_cols(x.est, x.pow))
+}
--- meta-mod-bias.R
+++ meta-mod-bias.R
@@ -18,67 +18,10 @@ source("sim-studies.R")
 # To simulate studies, use dataMA()
 # I'm a little surprised dataMA doesn't generate Hedges' g
 
-# dataMA() documentation:
-#Produces a dataset for meta-analysis. Applies both QRP
-#and selection at a proportion specified by propB if 
-#sel and QRP are 1 not 0. 
-
-#' @param k the number of studies in the MA
-#' @param QRP 1 if QRP/p-hacks are available, 0 otherwise
-#' @param sel 1 if publication bias selection exists, 0 otherwise
-#' @param propB the proportion of the sample affected by bias
-#' @param meanD the true effect (or the average of the true effects if heterogeneity exists)
-#' @param sigma the SD around the true effect
-#' @param cbdv the correlation between the multiple DVs
-#' @param maxN the max possible group size that could be created *this needs to be set higher than what can actually be generated--it doesn't mean you get bigger samples
-#' @param minN the min of the truncated normal for sample size
-#' @param meanN the mean of the truncated normal for sample size
-#' @param sdN the SD of the truncated normal for sample size
-#' @param multDV 1 if multiple DVs as a hack, 0 otherwise
-#' @param out 1 if optional outlier removal as a hack, 0 otherwise
-#' @param mod 1 if optional moderator as a hack, 0 otherwise
-#' @param colLim number of times to try collecting more data
-#' @param add number to add to each group when collecting more data
-#' @param verbose Should informations be printed?
-#' 
-
-# Make a function that runs dataMA three times,
-# once for each population,
-# then mixes them together
-# TODO: Note that other parameters might be worth exploring
-#       Expand argument assignment and passing
-modMA <- function(k, d, QRP, sel, propB) {
-  d1 <- dataMA(k = k, 
-               QRP = QRP, sel = sel, propB = propB, 
-               meanD = d[1], sigma = 0,
-               cbdv = .5, maxN = 200, minN = 20,
-               meanN = 50, sdN = 20,
-               multDV = 0, out = 0, mod = 0,
-               colLim = 0, add = 0, verbose = T)
-  d2 <- dataMA(k = k, 
-               QRP = QRP, sel = sel, propB = propB, 
-               meanD = d[2], sigma = 0,
-               cbdv = .5, maxN = 200, minN = 20,
-               meanN = 50, sdN = 20,
-               multDV = 0, out = 0, mod = 0,
-               colLim = 0, add = 0, verbose = T)
-  d3 <- dataMA(k = k, 
-               QRP = QRP, sel = sel, propB = propB, 
-               meanD = d[3], sigma = 0,
-               cbdv = .5, maxN = 200, minN = 20,
-               meanN = 50, sdN = 20,
-               multDV = 0, out = 0, mod = 0,
-               colLim = 0, add = 0, verbose = T)
-  return(bind_rows(d1, d2, d3, .id = "id"))
-}
-# TODO: Convert column "id" to contrast coding, not dummy coding.
-
-
 # make example data set for testing & debugging
-set.seed(42069)
-meta1 <- modMA(k = 20, d = c(0, .3, .6), 
-               sel = 1, propB = .70, QRP = 0)
-
+# set.seed(42069)
+# meta1 <- modMA(k = 20, d = c(0, .3, .6), 
+#                sel = 1, propB = .70, QRP = 0)
 # m1 <- rma(yi = d, sei = se, data = meta1)
 # funnel(m1, pch = as.numeric(meta1$id))
 # m1.mod <- rma(yi = d, sei = se, mods = ~id, data = meta1)
@@ -92,121 +35,49 @@ meta1 <- modMA(k = 20, d = c(0, .3, .6),
 # What is the power to detect d ~ id?
 # By how much does that power drop when we filter for sig?
 
-# function to analyze results of dataMA
-inspectMA <- function(dataset) {
-  # basic model
-  rmamod <- rma(yi = d, sei = se, data = dataset)
-  # test for moderator
-  moderation.test <- rma(yi = d, sei = se, 
-                         mods = ~id, data = dataset)
-  # test for small-study effect
-  egger.test <- rma(yi = d, sei = se,
-                    mods = ~se, data = dataset)
-  # test for moderator after adjustment for small-study
-  # is it id + se or id * se?
-  joint.test.additive <- rma(yi = d, sei = se,
-                    mods = ~id + se, data = dataset)
-  joint.test.interactive <- rma(yi = d, sei = se,
-                             mods = ~id * se, data = dataset)
-  # not sure how to interpret these parameters in the interactive model
-  # test for moderator in hedges & vevea weight model
-  # TODO  
-  # return test results
-  out <- data.frame(d.obs = summary(rmamod)$b[1],
-                    se.obs = summary(rmamod)$se[1],
-                    d.p = summary(rmamod)$pval[1],
-                    # Moderators
-                    # Note that .1 is the intercept, reference group
-                    mod.b.obs.1 = summary(moderation.test)$b[1],
-                    mod.b.obs.2 = summary(moderation.test)$b[2],
-                    mod.b.obs.3 = summary(moderation.test)$b[3],
-                    mod.p.1 = summary(moderation.test)$pval[1],
-                    mod.p.2 = summary(moderation.test)$pval[2],
-                    mod.p.3 = summary(moderation.test)$pval[3],
-                    # Egger / PET
-                    d.obs.pet = summary(egger.test)$b[1],
-                    p.pet = summary(egger.test)$pval[1],
-                    b.egger = summary(egger.test)$b[2],
-                    p.egger = summary(egger.test)$pval[2]
-                    # TODO: joint PET-RMA tests
-                    )
-  # to be continued...
-  
-  
-  return(data.frame(out))
-}
-
-
 # testing that passing of arguments is working right
 set.seed(42069)
 testset <- modMA(k = 20, d = c(0, .3, .6), 
                  sel = 0, propB = 0, QRP = 0)
+# check contrasts
+testset$id
+# make funnels for each subgroup
 funnel(rma(yi = d, sei = se, data = testset, subset = id == 1))
 funnel(rma(yi = d, sei = se, data = testset, subset = id == 2))
 funnel(rma(yi = d, sei = se, data = testset, subset = id == 3))
+# simulate biased data
 testset.70 <- modMA(k = 20, d = c(0, .3, .6), 
                     sel = 1, propB = .70, QRP = 0)
+# check contrasts
+testset.70$id
+# make funnels for each subgroup
 funnel(rma(yi = d, sei = se, data = testset.70, subset = id == 1))
 funnel(rma(yi = d, sei = se, data = testset.70, subset = id == 2))
 funnel(rma(yi = d, sei = se, data = testset.70, subset = id == 3))
 
 # Run simulations ----
-# TODO: It would be smart to turn this loop into a function,
-#       then call the function instead of copying and pasting.
 # TODO: It would also be smart to save the output of summarize_run to an object
 #       but maybe we work with the raw output objects for now
 
-
-runStudy <- function(nSim, k, d, sel, propB, QRP) {
-  output <- NULL; 
-  for (i in 1:nSim) {
-    # make a dataset of simulated studies
-    # k studies each, effect sizes given by d
-    tempdata <- modMA(k = k, d = d, 
-                      sel = sel, propB = propB, QRP = QRP)
-    # fit our various meta-analytic models to the simulated data
-    tempresult <- inspectMA(tempdata)
-    # add this iteration's results to the output object
-    output <- bind_rows(output, tempresult)
-  }
-  output
-}
-  
-runStudy(nSim = 3, k = 20, d = c(0, .3, .6), sel = 0, propB = 0, QRP = 0)
-
 # set 1 : no bias
-output.nobias <- runStudy(nSim = 3, k = 20, d = c(0, .3, .6), 
+output.nobias <- runStudy(nSim = 100, k = 20, d = c(0, .3, .6), 
                           sel = 0, propB = 0, QRP = 0)
 output.nobias
 
 # set 2: publication bias, 70+% of each subgroup stat. sig
-output.70p_pubbias <- runStudy(nSim = 3, k = 20, d = c(0, .3, .6), 
+output.70p_pubbias <- runStudy(nSim = 100, k = 20, d = c(0, .3, .6), 
                                sel = 1, propB = .70, QRP = 0)
 output.70p_pubbias
 
-# functions for summarizing a simulation series in terms of mean estimates and
-#    error rates
-is_sig <- function(x) x < .05
+# set 3: subtler differences
+output.smallfx.nobias <- runStudy(nSim = 100, k = 20, d = c(0, .2, .4), 
+                                  sel = 0, propB = 0, QRP = 0)
 
-summarize_run <- function(x) {
-  # Get estimates of d, moderator parameters, bias parameter, bias-adjusted PET
-  x.est <- x %>% 
-    summarize_each(funs(mean), d.obs, se.obs, mod.b.obs.1:mod.b.obs.3,
-                   d.obs.pet, b.egger) %>% 
-    # make cell means (NOTE: ASSUMES DUMMY CODING)
-    # Might be a simpler way... lsmeans package?
-    mutate(d1.obs = mod.b.obs.1,
-           d2.obs = mod.b.obs.1 + mod.b.obs.2,
-           d3.obs = mod.b.obs.1 + mod.b.obs.3)
+# set 4: subtler differences + 70% of results are stat. sig
+output.smallfx.70p_pubbias <- runStudy(nSim = 100, k = 20, d = c(0, .2, .4), 
+                                       sel = 1, propB = 0.7, QRP = 0)
 
-  # Get power (or Type I) rates for each p-value test
-  x.pow <- x %>% 
-    summarize_each(funs(mean(is_sig(.))), 
-                   d.p, mod.p.1:mod.p.3, p.egger)
-  
-  return(bind_cols(x.est, x.pow))
-}
-
+# summarize results of set 1 and set 2
 summarize_run(output.nobias) 
 # estimates: d = .00, .30, .60; power, 4.2%, 92%, 100%; egger type 1, 11%
 # PET shows strong downward bias, d = .14
@@ -215,14 +86,25 @@ summarize_run(output.70p_pubbias)
 # estimates: d = .45, .58, .74; power, 100%, 15%, 91%; egger power, 76%
 # note that differences between subpopulations have been halved
 
+summarize_run(output.smallfx.nobias) 
+# observed d = 0, .2, .4; power = 5.6%, 60%, 99%; egger type 1 6.4%
+summarize_run(output.smallfx.70p_pubbias) # dramatically reduced power
+# observed d = .45, .54, .63; power = 100%, 2.2%, 35%; egger power 74%
+
+
+
+
+
+# This area under construction
+
 # Plot
 output.nobias$bias <- "No Bias"
 output.70p_pubbias$bias <- "70% sig"
-# Parameterized as cell means
+# Parameterized as cell means (contrast coding)
 bind_rows(output.nobias, output.70p_pubbias) %>% 
-  mutate(d1.obs = mod.b.obs.1,
-         d2.obs = mod.b.obs.1 + mod.b.obs.2,
-         d3.obs = mod.b.obs.1 + mod.b.obs.3) %>% 
+  mutate(d1.obs = mod.b.obs.1 + mod.b.obs.2,
+         d2.obs = mod.b.obs.1 + mod.b.obs.3,
+         d3.obs = mod.b.obs.1 - mod.b.obs.2 - mod.b.obs.3) %>% 
   gather(key, value, d1.obs:d3.obs) %>% 
   ggplot(aes(x = value)) +
   geom_histogram() +
@@ -235,7 +117,7 @@ bind_rows(output.nobias, output.70p_pubbias) %>%
   ggplot(aes(x = value)) +
   geom_histogram() +
   facet_grid(bias ~ key) +
-  scale_x_continuous(limits = c(-.2, 1))  +
+  scale_x_continuous(limits = c(-1, .5))  +
   ggtitle("Moderation between d = 0, 0.3, 0.6")
 
 # Next steps:
@@ -246,26 +128,14 @@ bind_rows(output.nobias, output.70p_pubbias) %>%
 hist(meta1$N)
 # implement PET-RMA model
 
-# what if differences are more subtle?
-output.smallfx.nobias <- runStudy(nSim = 3, k = 20, d = c(0, .2, .4), 
-                                  sel = 0, propB = 0, QRP = 0)
-
-output.smallfx.70p_pubbias <- runStudy(nSim = 3, k = 20, d = c(0, .2, .4), 
-                                       sel = 1, propB = 0.7, QRP = 0)
-
-summarize_run(output.smallfx.nobias) 
-# observed d = 0, .2, .4; power = 5.6%, 60%, 99%; egger type 1 6.4%
-summarize_run(output.smallfx.70p_pubbias) # dramatically reduced power
-# observed d = .45, .54, .63; power = 100%, 2.2%, 35%; egger power 74%
-
 # Plot
 output.smallfx.nobias$bias <- "No Bias"
 output.smallfx.70p_pubbias$bias <- "70% sig"
 # Parameterized as cell means
 bind_rows(output.smallfx.nobias, output.smallfx.70p_pubbias) %>% 
-  mutate(d1.obs = mod.b.obs.1,
-         d2.obs = mod.b.obs.1 + mod.b.obs.2,
-         d3.obs = mod.b.obs.1 + mod.b.obs.3) %>% 
+  mutate(d1.obs = mod.b.obs.1 + mod.b.obs.2,
+         d2.obs = mod.b.obs.1 + mod.b.obs.3,
+         d3.obs = mod.b.obs.1 - mod.b.obs.2 - mod.b.obs.3) %>% 
   gather(key, value, d1.obs:d3.obs) %>% 
   ggplot(aes(x = value)) +
   geom_histogram() +
